568803a7f37f924840b594b3a0d786804aa4b231
ksawant1 <54869724+ksawant1@users.noreply.github.com>
2020-07-22 04:44:42 +0530

    common ints chooses common integers in the array list objects and prints in results

---
+++ arrayvisitors/src/arrayvisitors/visitors/CommonIntsVisitor.java
@@ -0,0 +1,41 @@
+package arrayvisitors.visitors;
+import arrayvisitors.adt.MyArray;
+import arrayvisitors.adt.MyArrayList;
+import arrayvisitors.util.Results;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * a visitor class that finds common integers stored in myarraylist and sends to result for printing
+ */
+public class CommonIntsVisitor implements Visitor{
+	Results results;
+
+	//sets result instance
+	public CommonIntsVisitor(Results results) {
+		this.results = results;
+	}
+
+	//mainly for calling findCommonIntsVisitor()
+	public void visit(Element myElement){
+		findCommonIntsVisitor((MyArrayList) myElement);
+	}
+
+	private void findCommonIntsVisitor(MyArrayList myArrayList) {
+		Set<Integer> integers = new LinkedHashSet<>();
+		Set<Integer> commonIntegers = new LinkedHashSet<>();
+
+		// for finding common integers and passing to results
+		for (MyArray array : myArrayList.getArrays()) {
+			for (int x : array.getArray()) {
+
+				if (integers.contains(x))
+					commonIntegers.add(x);
+
+				integers.add(x);
+			}
+		}
+		results.storeCommonInteger(commonIntegers);
+	}
+
+}

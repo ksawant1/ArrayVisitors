35a2970b3474acbc9ed750d689d2be5b4124baac
ksawant1 <54869724+ksawant1@users.noreply.github.com>
2020-07-22 04:48:17 +0530

    interface for myarraylist

---
+++ arrayvisitors/src/arrayvisitors/adt/MyArrayListI.java
@@ -0,0 +1,8 @@
+package arrayvisitors.adt;
+
+/**
+ * interface for functions of Myarraylist
+ */
+public interface MyArrayListI{
+	public MyArray[] getArrays() ;
+}
--- arrayvisitors/src/arrayvisitors/visitors/Element.java
+++ arrayvisitors/src/arrayvisitors/visitors/Element.java
@@ -2,7 +2,7 @@ package arrayvisitors.visitors;
 
 /**
  * Element interface with accept method
- * both array and arraylist are of type element
+ * both array and arraylist are of type elementaacca
  */
 public interface Element {
 	public void accept(Visitor visit);
